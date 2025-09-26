053a2a5317a8083e430b5d8d26fd81b10c106f6b
Valerie Ko <valerie.ktn@gmail.com>
2025-09-26 13:11:33 +0800

    Commit main.py without the largest_companies.csv

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model=OllamaLLM(model="llama3.2")
+template="""
+    You are an expert in answering questions about the 100 largest companies in the US
+
+    Here are some relevant information about the companies:{info}
+
+    Here is a question about the companies: {question}
+"""
+
+prompt=ChatPromptTemplate.from_template(template)
+chain=prompt | model
+
+result=chain.invoke({"info":[],"question":"Which company has the highest revenue?"})
+print(result)
